c3794b451b7609197f1bd79b4cd66ad1969b9ce3
Amir-Aram <wannabeccie5@gmail.com>
2022-01-18 14:38:11 +0100

    while loops in bash

--- shell_script/while_loops.sh
+++ shell_script/while_loops.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 #incremental loop:
-#i=0
-#while [ $i -le 10 ]
-#do
-#	echo $i
-#	((i++))
-#done
+i=0
+while [ $i -le 10 ]
+do
+	echo $i
+	((i++))
+done
 #####################################
-#infinit while loop:
-#i=0
-#while [ True ]
-#do
-#	echo $i
-#done
+
+#infinit while loop: push (ctr + c) to break the loop after running 
+i=0
+while [ True ]
+do
+	echo $i
+done
 
