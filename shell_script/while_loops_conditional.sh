8ec023f4fb34be472e3c1b160929bb33891a4055
Amir-Aram <wannabeccie5@gmail.com>
2022-01-18 14:40:21 +0100

    conditional for loop

--- shell_script/while_loops_conditional.sh
+++ shell_script/while_loops_conditional.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#for i in {1..10}
-#do
-#    if [ $i -gt 5 ]
-#    then
-#	break
-#    fi
-#    echo $i
-#done
+for i in {1..10}
+do
+    if [ $i -gt 5 ]
+    then
+	break
+    fi
+    echo $i
+done
 
+#here comes the condition
 for i in {1..10}
 do
     if [ $i -eq 2 ] || [ $i -eq 5 ]
